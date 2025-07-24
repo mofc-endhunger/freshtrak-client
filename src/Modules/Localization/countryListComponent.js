f51aa73a9db4182bef5def11d347ef2afaa51800
Alex Shvyrev <ashvyrev@mofc.org>
2025-07-24 09:44:46 -0400

    fixed localization dropdown

--- src/Modules/Localization/countryListComponent.js
+++ src/Modules/Localization/countryListComponent.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { Dropdown } from 'semantic-ui-react'
+import React from "react";
+import { Dropdown } from "semantic-ui-react";
 
 const countryOptions = [
-  { key: 'en', value: 'en', flag: 'us', text: 'English' },
-  { key: 'spa', value: 'spa', flag: 'mx', text: 'Spanish' },
-  { key: 'som', value: 'som', flag: 'so', text: 'Somali' },
-  { key: 'rus', value: 'rus', flag: 'ru', text: 'Russian' },
-  { key: 'tur', value: 'tur', flag: 'tr', text: 'Turkish' },
-  { key: 'ara', value: 'ara', flag: 'ae', text: 'Arabic' },
-  { key: 'zho', value: 'zho', flag: 'cn', text: 'China' },
-  { key: 'hin', value: 'hin', flag: 'in', text: 'Hindi' },
-  { key: 'nep', value: 'nep', flag: 'np', text: 'Nepali' }
+	{ key: "en", value: "en", flag: "us", text: "English" },
+	{ key: "spa", value: "spa", flag: "mx", text: "Spanish" },
+	{ key: "som", value: "som", flag: "so", text: "Somali" },
+	{ key: "rus", value: "rus", flag: "ru", text: "Russian" },
+	{ key: "tur", value: "tur", flag: "tr", text: "Turkish" },
+	{ key: "ara", value: "ara", flag: "ae", text: "Arabic" },
+	{ key: "zho", value: "zho", flag: "cn", text: "China" },
+	{ key: "hin", value: "hin", flag: "in", text: "Hindi" },
+	{ key: "nep", value: "nep", flag: "np", text: "Nepali" },
+];
 
-]
-
-const CountryListComponent = (props) => (
-  <Dropdown compact
-    onChange={props.change}
-    placeholder='Select Language'
-    search
-    selection
-    options={countryOptions}
-  />
-)
+const CountryListComponent = props => (
+	<Dropdown
+		onChange={props.change}
+		placeholder="Select Language"
+		search
+		selection
+		options={countryOptions}
+	/>
+);
 
 export default CountryListComponent;

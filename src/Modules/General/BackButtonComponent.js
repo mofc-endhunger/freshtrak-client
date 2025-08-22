147d53d92416a599f59fb79d110b3c063ae504ca
Alex Shvyrev <ashvyrev@mofc.org>
2025-08-22 15:42:58 -0400

    fixed back button functonality

--- src/Modules/General/BackButtonComponent.js
+++ src/Modules/General/BackButtonComponent.js
@@ -1,13 +1,28 @@
 import React, { Fragment } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import back from "../../Assets/img/back.svg";
 import "../../Assets/scss/main.scss";
 
 const BackButtonComponent = () => {
 	const navigate = useNavigate();
+	const location = useLocation();
+
 	const backHome = () => {
-		navigate(-1);
+		const currentPath = location.pathname;
+		const pathParts = currentPath.split("/");
+		const hasTimeslotId = pathParts.length === 5; // /register/form/:eventDateId/:timeslotId (5 parts due to leading empty string)
+
+		if (hasTimeslotId) {
+			// Extract eventDateId from the URL (it's the 4th part, index 3)
+			const eventDateId = pathParts[3];
+			navigate(`/register/event/${eventDateId}`);
+		} else {
+			// If no timeslot ID, extract eventDateId from the 4th part (index 3)
+			const eventDateId = pathParts[3];
+			navigate(`/register/event/${eventDateId}`);
+		}
 	};
+
 	return (
 		<Fragment>
 			<div className="row">

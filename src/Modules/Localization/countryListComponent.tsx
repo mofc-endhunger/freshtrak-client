25fb9658c9d132e77b4cec4dd91f859b77323e50
Alexander Shvyrev <ashvyrev@mofc.org>
2025-11-05 15:47:26 -0500

    increased z-index for countrylist

--- src/Modules/Localization/countryListComponent.tsx
+++ src/Modules/Localization/countryListComponent.tsx
@@ -50,7 +50,7 @@ const CountryListComponent: React.FC<CountryListComponentProps> = props => {
 			<SelectTrigger className="bg-white text-gray-400 border border-white rounded px-2 md:px-3 py-1 text-xs md:text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 h-auto min-w-[140px]">
 				<SelectValue placeholder="Select Language" />
 			</SelectTrigger>
-			<SelectContent className="bg-white">
+			<SelectContent className="bg-white z-[10000]">
 				{countryOptions.map(option => (
 					<SelectItem
 						key={option.key}

50675959c47a836418ae56c98de3fe74b6b307ee
Alexander Shvyrev <ashvyrev@mofc.org>
2025-12-01 16:21:13 -0500

    added link to footer logo that takes the user back to / page

--- src/Modules/General/LogoComponent.js
+++ src/Modules/General/LogoComponent.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import FooterLogoIcon from "../../Assets/img/footer-logo.svg";
+import { RENDER_URL } from "../../Utils/Urls";
 
 const LogoComponent = () => {
 	return (
 		<div className="w-full lg:w-1/2 xl:w-1/2">
-			<div className="h-10">
-				<img
-					src={FooterLogoIcon}
-					alt="Freshtrak Logo"
-					className="max-w-full max-h-full"
-				/>
-			</div>
+			<Link to={RENDER_URL.ROOT_URL} className="block">
+				<div className="h-10">
+					<img
+						src={FooterLogoIcon}
+						alt="Freshtrak Logo"
+						className="max-w-full max-h-full"
+					/>
+				</div>
+			</Link>
 		</div>
 	);
 };
